3a6c149af94248bda4ab9656c3ab2645bcb4405a
areynare <a.reyna@uniandes.edu.co>
2024-03-11 17:06:53 -0600

    fix

--- POCArquitectura/src/bff_sistemas_externos/api/servicios_externos.py
+++ POCArquitectura/src/bff_sistemas_externos/api/servicios_externos.py
@@ -26,8 +26,8 @@ def agregar_propiedad():
     
     try:
         propiedad_data = request.json
-        saga.execute(propiedad_data)
-        return Response('{}', status=202, mimetype='application/json')
+        #saga.execute(propiedad_data)
+        return Response('{propiedad_data}', status=202, mimetype='application/json')
     except Exception as e:
         return Response(json.dumps(dict(error=str(e))), status=400, mimetype='application/json')
 
