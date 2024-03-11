adccac96db5686c021faeacf773a1cc20d2fcb18
areynare <a.reyna@uniandes.edu.co>
2024-03-11 00:53:54 -0600

    change pulsar

--- POCArquitectura/src/bff_sistemas_externos/api/utils.py
+++ POCArquitectura/src/bff_sistemas_externos/api/utils.py
@@ -7,9 +7,9 @@ def revisar_token():
         partes = authorization_header.split()
         if len(partes) == 2 and partes[0] == 'Bearer':
             token = partes[1]
-            url_otra_api = 'http://127.0.0.1:3000/autenticador/usuario'
+            url_usuarios = 'http://127.0.0.1:3000/autenticador/usuario'
             headers = {'Authorization': f'Bearer {token}'}
-            response = requests.get(url_otra_api, headers=headers)
+            response = requests.get(url_usuarios, headers=headers)
             if response.status_code == 200:
                 return token
             else:
