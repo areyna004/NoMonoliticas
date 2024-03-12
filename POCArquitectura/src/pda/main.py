20390c53846dd9cb11f9fae20e0b9a8b08dec77a
areynare <a.reyna@uniandes.edu.co>
2024-03-11 19:28:38 -0600

    fix

--- POCArquitectura/src/pda/main.py
+++ POCArquitectura/src/pda/main.py
@@ -41,6 +41,7 @@ def consumir_comandos():
                 dto_final = sr.crear_propiedad(propiedad_dto)
                 producer.send(dto_final.to_json().encode('utf-8'))
             if propiedad_externo['accion'] == 'eliminar': 
+                print('aqui *********************************')
                 dto_final = sr.eliminar_propiedad(propiedad_dto)
                 producer2.send(dto_final.to_json().encode('utf-8'))
             consumer.acknowledge(msg)
